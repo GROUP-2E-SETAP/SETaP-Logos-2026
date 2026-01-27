0f729b3c1529d63f86f875ba5661be286e07c010
Goff120 <geoffreysmithuk@icloud.com>
2026-01-27 15:48:55 +0000

    made top left sheild

--- import turtle.py
+++ import turtle.py
@@ -1,13 +1,26 @@
 import turtle
 
+def mid_top():
+    t.home()
+    t.penup()
+    t.left(90)
+    t.forward(200)
+    t.pendown()
+
+def left_sheild():
+    mid_top()
+    t.left(215)
+    t.circle(150, 90)
+
+
+def sheild():
+    left_sheild()
+    #right_sheild()
+
+
+
 screan = turtle.Screen()
 t = turtle.Turtle()
 
-t.penup()
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.pendown()
-
+sheild()
 turtle.done()
